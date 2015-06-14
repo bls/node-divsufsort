e127432f258b2236cdba98832f88a0cd6d78b0ee
bls <blair.strang@gmail.com>
2015-06-14 21:19:24 +1000

    Got divbwt working.

--- src/divsufsort.cc
+++ src/divsufsort.cc
@@ -17,12 +17,6 @@ using v8::Number;
 using node::Buffer;
 
 /*
-  Ideas: allow caller to pass a pre-allocated buffer if they want; 
-  reasonable allocations might be "quite large". If output buffer is
-  not big enough, extend it.
-*/
-
-/*
   saint_t divsufsort(const sauchar_t *T, saidx_t *SA, saidx_t n);
 */
 NAN_METHOD(divsufsort_) {
@@ -59,7 +53,7 @@ NAN_METHOD(divsufsort_) {
 
   saint_t ret = divsufsort(T, SA, tLen);
   if(ret < 0) {
-    return NanThrowError("Suffix array construction failed");
+    return NanThrowError("divsufsort failed");
   }
   NanReturnValue(NanNew<Number>(0));
 }
@@ -67,7 +61,52 @@ NAN_METHOD(divsufsort_) {
 
 NAN_METHOD(divbwt_) {
   NanScope();
-  NanReturnUndefined();
+
+  if (args.Length() != 3) {
+    return NanThrowTypeError("Wrong number of arguments");
+  }
+  Local<Value> arg0 = args[0];
+  Local<Value> arg1 = args[1];
+  Local<Value> arg2 = args[2];
+
+  if(!Buffer::HasInstance(arg0)) {
+    return NanThrowTypeError("First argument must be a Buffer");
+  }
+  if(!Buffer::HasInstance(arg1)) {
+    return NanThrowTypeError("Second argument must be a Buffer");
+  }
+  if(!Buffer::HasInstance(arg2)) {
+    return NanThrowTypeError("Third argument must be a Buffer");
+  }
+
+  Handle<Object> tObj = arg0->ToObject();
+  Handle<Object> uObj = arg1->ToObject();
+  Handle<Object> aObj = arg2->ToObject();
+
+  size_t tLen = Buffer::Length(tObj);
+  size_t uLen = Buffer::Length(uObj);
+  size_t aLen = Buffer::Length(aObj);
+
+  if(sizeof(saidx_t) != 4 || sizeof(sauchar_t) != 1) {
+    return NanThrowError("Internal error: unexpected types");
+  }
+  if(tLen != uLen) {
+    return NanThrowError("First and second args must be same size");
+  }
+  if(aLen < (sizeof(saidx_t) * tLen)) { // TODO: integer overflow
+    return NanThrowError("Third buffer too small");
+  }
+
+  sauchar_t *T = reinterpret_cast<sauchar_t *>(Buffer::Data(tObj));
+  sauchar_t *U = reinterpret_cast<sauchar_t *>(Buffer::Data(uObj));
+  saidx_t *AUX = reinterpret_cast<saidx_t *>(Buffer::Data(aObj));
+
+  saidx_t ret = divbwt(T, U, AUX, tLen);
+  if(ret < 0) {
+    return NanThrowError("divbwt failed");
+  }
+
+  NanReturnValue(NanNew<Number>(ret));
 }
 
 /*
--- test/native.js
+++ test/native.js
@@ -12,14 +12,14 @@ function bufToUint32Array(buf) {
 }
 
 describe('native', function() {
-    describe('module', function() {
-        it('should export the right functions', function() {
+    describe('module', function () {
+        it('should export the right functions', function () {
             expect(divsufsort).to.be.a('function');
             expect(divbwt).to.be.a('function');
         });
     });
-    describe('divsufsort', function() {
-        it('should accept exactly 2 arguments', function() {
+    describe('divsufsort', function () {
+        it('should accept exactly 2 arguments', function () {
             var a = new Buffer(0),
                 b = new Buffer(0),
                 c = new Buffer(0);
@@ -40,7 +40,7 @@ describe('native', function() {
                 divsufsort(a, b, c);
             }).to.throw(TypeError);
         });
-        it('should not accept non-buffer argument types', function() {
+        it('should not accept non-buffer argument types', function () {
             var testArgs = [null, undefined, -1, 'test', NaN, {}];
             _.forEach(testArgs, function (arg1) {
                 _.forEach(testArgs, function (arg2) {
@@ -51,14 +51,14 @@ describe('native', function() {
             });
         });
         /*
-        it('should reject the same buffer as both input and output', function() {
-            var a = new Buffer(0);
-            expect(function () {
-                divsufsort.divsufsort(a, a);
-            }).to.throw(Error);
-        });
-        */
-        it('should check buffer sizes', function() {
+         it('should reject the same buffer as both input and output', function() {
+         var a = new Buffer(0);
+         expect(function () {
+         divsufsort.divsufsort(a, a);
+         }).to.throw(Error);
+         });
+         */
+        it('should check buffer sizes', function () {
             var a = new Buffer('abba'),
                 b = new Buffer(15),  // size < 4 * n
                 c = new Buffer(16);  // size == 4 * n
@@ -69,14 +69,14 @@ describe('native', function() {
                 divsufsort(a, c);
             }).to.not.throw(Error);
         });
-        it('should handle the trivial case', function() {
+        it('should handle the trivial case', function () {
             var a = new Buffer('a');
             var b = new Buffer('zzzz'); // Junk
             expect(b.readUInt32LE(0)).to.not.be.equal(0);
             divsufsort(a, b);
             expect(b.readUInt32LE(0)).to.be.equal(0);
         });
-        it('should handle a simple case', function() {
+        it('should handle a simple case', function () {
             var t = new Buffer('abracadabra'),
                 sa = new Buffer(4 * t.length);
             var ret = divsufsort(t, sa);
@@ -84,6 +84,53 @@ describe('native', function() {
             expect(bufToUint32Array(sa)).to.be.eql([
                 10, 7, 0, 3, 5, 8, 1, 4, 6, 9, 2
             ]);
+        });
+    });
+
+    describe('divbwt', function () {
+        it('should accept exactly 3 arguments', function () {
+            var a = new Buffer(0),
+                b = new Buffer(0),
+                c = new Buffer(0);
+
+            expect(function () {
+                divsufsort();
+            }).to.throw(TypeError);
+
+            expect(function () {
+                divsufsort(a);
+            }).to.throw(TypeError);
+
+            expect(function () {
+                divsufsort(a, b);
+            }).to.not.throw(TypeError);
+
+            expect(function () {
+                divsufsort(a, b, c);
+            }).to.throw(TypeError);
+        });
+        it('should not accept non-buffer argument types', function () {
+            var testArgs = [null, undefined, -1, 'test', NaN, {}];
+            _.forEach(testArgs, function (arg1) {
+                _.forEach(testArgs, function (arg2) {
+                    _.forEach(testArgs, function(arg3) {
+                        expect(function () {
+                            divbwt(arg1, arg2, arg3);
+                        }).to.throw(TypeError);
+                    });
+                });
+            });
+        });
+        // TODO: Buffer size checks
+        it('should handle a simple case', function() {
+            var t = new Buffer('banana'),
+                u = new Buffer(t.length),
+                aux = new Buffer(4 * t.length);
+            console.log(t.length);
+            var ret = divbwt(t, u, aux);
+            var us = u.toString(),
+                result = us.slice(0, ret) + '$' + us.slice(ret);
+            expect(result).to.be.equal('annb$aa');
         });
     });
 });
