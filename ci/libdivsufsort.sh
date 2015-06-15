7f7026c741fbe34a11f4f031dd6ddfa7ff8725a0
bls <blair.strang@covata.com>
2015-06-15 12:21:14 +1000

    See if non-HEAD version of libdivsufsort works for CI.

--- ci/libdivsufsort.sh
+++ ci/libdivsufsort.sh
@@ -4,6 +4,7 @@ mkdir ~/build
 cd ~/build
 git clone https://github.com/y-256/libdivsufsort libdivsufsort
 cd libdivsufsort
+git checkout tags/2.0.1
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX="/usr/local" ..
